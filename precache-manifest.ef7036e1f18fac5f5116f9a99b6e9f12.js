be7c11338e2012b06d9c76d6ed0789157d215b20
mdera <mdera@wakacje.pl>
2020-01-03 22:38:15 +0100

    Updates

---
+++ precache-manifest.ef7036e1f18fac5f5116f9a99b6e9f12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "986e4bb09ab9575d3b83e9e1df99f169",
+    "url": "/peppa/index.html"
+  },
+  {
+    "revision": "4965e69ce57444a1de15",
+    "url": "/peppa/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "4965e69ce57444a1de15",
+    "url": "/peppa/static/js/2.46458ad5.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/peppa/static/js/2.46458ad5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "74e3ab7042ea8bf9435d",
+    "url": "/peppa/static/js/main.b947e66b.chunk.js"
+  },
+  {
+    "revision": "cfcc4be7563f807788e7",
+    "url": "/peppa/static/js/runtime-main.24d91118.js"
+  }
+]);
