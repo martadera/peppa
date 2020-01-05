ccd92b9a64d632eeaffc22c87a7cd6cf4a2892ca
mdera <mdera@wakacje.pl>
2020-01-05 11:00:21 +0100

    Updates

--- precache-manifest.acea062084336d07c85e27cc7a342aff.js
+++ precache-manifest.acea062084336d07c85e27cc7a342aff.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "adcf0a90a87929dd98cc693879ccf0c1",
+    "revision": "a5f9528748c7d57bd512d27986b4de0c",
     "url": "/peppa/index.html"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/peppa/static/js/2.46458ad5.chunk.js.LICENSE"
   },
   {
-    "revision": "4374a060b8b8e997d6a7",
-    "url": "/peppa/static/js/main.062e621c.chunk.js"
+    "revision": "f0a1b8c5b31f738602e9",
+    "url": "/peppa/static/js/main.487d917d.chunk.js"
   },
   {
     "revision": "cfcc4be7563f807788e7",
